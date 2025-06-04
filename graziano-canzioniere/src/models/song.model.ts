479a2c3e178864bd75df29e384d65e9d120e5fe1
clan-graziano <159022062+clan-graziano@users.noreply.github.com>
2025-06-04 18:31:41 +0000

    secondo punto

---
+++ graziano-canzioniere/src/models/song.model.ts
@@ -0,0 +1,11 @@
+export class Song {
+    data_uscita : string
+    durata : string
+    title : string
+
+    constructor(data_uscita : string, durata : string, title : string){
+        this.data_uscita = data_uscita
+        this.durata = durata
+        this.title = title
+    }
+}
